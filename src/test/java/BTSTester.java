b0dad20a4275feba43c02fbd967821a07d49a198
Tomasz Lewowski <tomasz.lewowski@wp.eu>
2014-03-20 22:46:43 +0100

    Adding sources

---
+++ src/test/java/BTSTester.java
@@ -0,0 +1,14 @@
+import junit.framework.Assert;
+import org.junit.Test;
+
+/**
+ * Created by Ja on 20.03.14.
+ */
+public class BTSTester {
+
+    @Test
+    public void testsPass()
+    {
+        Assert.assertEquals(1,1);
+    }
+}
