a615a143445685dfaa2c7adb1b4136da759e169c
Tomasz Lewowski <tomasz.lewowski@wp.eu>
2014-03-23 21:22:06 +0100

    Added Mockito to dependencies + performed sanity checks

--- src/test/java/BTSTester.java
+++ src/test/java/BTSTester.java
@@ -1,14 +1,26 @@
 import junit.framework.Assert;
 import org.junit.Test;
-
+import java.util.Iterator;
+import static org.mockito.Mockito.*;
 /**
  * Created by Ja on 20.03.14.
  */
+
+
 public class BTSTester {
 
     @Test
-    public void testsPass()
+    public void sanityCheckForJUnit()
     {
         Assert.assertEquals(1,1);
+    }
+
+    @Test
+    public void sanityCheckForMockito()
+    {
+        Iterator i = mock(Iterator.class);
+        when(i.next()).thenReturn("Hello").thenReturn("Mockito");
+        String res = i.next() + " " + i.next();
+        Assert.assertEquals(res, "Hello Mockito");
     }
 }
