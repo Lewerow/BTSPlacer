8ddc68670dfb3b36f165a8467d3d01333117c232
Daniel Karwacki <vortim@o2.pl>
2014-04-10 11:58:41 +0200

    Bts drawing proper scaling (assumed that ragne is radius not diameter)

--- src/main/java/views/btsrender/BtsImageRenderer.java
+++ src/main/java/views/btsrender/BtsImageRenderer.java
@@ -30,13 +30,8 @@ public class BtsImageRenderer implements IBtsRenderer {
 		double x = location.getX();
 		double y = location.getY();
 
-		// FIXME scaling of image must depend on strength of provided signal !!!
-		// Image img, int x, int y,
-		// int width, int height,
-		// ImageObserver observer
-
 		int range = bts.getRange();
 
-		g.drawImage(btsImage, (int) x - range / 2, (int) y - range / 2, range, range, null);
+		g.drawImage(btsImage, (int) x - range, (int) y - range, range * 2, range * 2, null);
 	}
 }
