5b02df1672a7335490348ef48eb874afc5661b7a
Daniel Karwacki <vortim@o2.pl>
2014-06-02 23:12:03 +0200

    Drawing of subscribers fix - there was private static field with "maxSignalRequested" ...

--- src/main/java/calculations/SubscriberCenter.java
+++ src/main/java/calculations/SubscriberCenter.java
@@ -1,16 +1,17 @@
 package calculations;
 
-import java.awt.Color;
-
-import org.javatuples.Pair;
-
-import processing.core.PApplet;
-import processing.core.PGraphics;
 import de.fhpotsdam.unfolding.UnfoldingMap;
 import de.fhpotsdam.unfolding.geo.Location;
 import de.fhpotsdam.unfolding.marker.AbstractMarker;
+import de.fhpotsdam.unfolding.marker.Marker;
 import de.fhpotsdam.unfolding.utils.GeoUtils;
 import de.fhpotsdam.unfolding.utils.ScreenPosition;
+import org.javatuples.Pair;
+import processing.core.PApplet;
+import processing.core.PGraphics;
+import views.map.MapApplet;
+
+import java.awt.*;
 
 /**
  * Created by Ja on 11.05.14. Assumption: SubscriberCenter represents center of
@@ -21,19 +22,12 @@ public class SubscriberCenter extends AbstractMarker {
     private Pair<Double, Double> variance;
 
     private static final Color activeColor = Color.green;
-    private static final int maxSignalRequested = 3000;
     private final float diameterUnit = 0.5f;
 
     public SubscriberCenter(Double requiredSignal, Location center, Double sigmaX, Double sigmaY) {
         this.requiredSignal = requiredSignal;
         this.location = center;
         this.variance = new Pair<Double, Double>(sigmaX, sigmaY);
-    }
-
-    public SubscriberCenter(Double requiredSignal, Location center, Pair<Double, Double> variance) {
-        this.requiredSignal = requiredSignal;
-        this.location = center;
-        this.variance = variance;
     }
 
     private float getDistance(Location mainLocation, float size, UnfoldingMap map) {
@@ -66,7 +60,7 @@ public class SubscriberCenter extends AbstractMarker {
     }
 
     private void drawGradient(PGraphics p, float distance, float startAngle, float scopeAngle,
-            float x, float y, int layerCount) {
+                              float x, float y, int layerCount) {
         float distanceStep = distance / layerCount;
         float currentDistance = distanceStep;
         for (int i = layerCount; i > 0; i--) {
@@ -76,7 +70,7 @@ public class SubscriberCenter extends AbstractMarker {
     }
 
     private void drawCircularSubscribers(PGraphics p, float x, float y, float distance) {
-        int transparency = (int) (0xff * (requiredSignal / maxSignalRequested));
+        int transparency = (int) (0xff * (requiredSignal / MapApplet.maxRequestedSignalDrawn));
         p.fill(activeColor.getRGB(), transparency);
         drawGradient(p, distance, 0, 6.3f, x, y, 1);
     }
@@ -98,10 +92,6 @@ public class SubscriberCenter extends AbstractMarker {
         return requiredSignal;
     }
 
-    public void setRequiredSignal(Double requiredSignal) {
-        this.requiredSignal = requiredSignal;
-    }
-
     public Pair<Double, Double> getVariance() {
         return variance;
     }
@@ -110,6 +100,8 @@ public class SubscriberCenter extends AbstractMarker {
         this.variance = new Pair<Double, Double>(sigmaX, sigmaY);
     }
 
+    //TODO [DaKa] ToLe: better think if you are really want to return requiredSignal for SubscriberCenter,
+    // if yes then this method is useless (you have getRequiredSignal() method above)
     public double getMaxRequiredSignal() {
         return requiredSignal;
     }
